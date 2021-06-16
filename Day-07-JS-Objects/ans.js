d11c01a4e00de4ec0bed525868340b67b56fb8c6
CodeBlooded-RahulMaurya <rahulmaurya0001@gmail.com>
2021-06-17 01:52:02 +0530

    done

--- Day-07-JS-Objects/ans.js
+++ Day-07-JS-Objects/ans.js
@@ -13,6 +13,7 @@ var student = {
 
 getProperties(student);
 
+
 // 2. Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property. Sample object: var student = { name : "David Rayy", sclass : "VI", rollno : 12 };
 
 function delProp(jsObject , prop){
@@ -23,6 +24,7 @@ function delProp(jsObject , prop){
 
 delProp(student, "rollno");
 
+
 // 3. Write a JavaScript program to get the length of a JavaScript object.  Sample object : var student = { name : "David Rayy", sclass : "VI", rollno : 12 };
 
 function getLength(jsObject) {
@@ -30,6 +32,7 @@ function getLength(jsObject) {
 }
 
 getLength(student);
+
 
 // 4. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books. var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }];
 
@@ -49,6 +52,7 @@ var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: 
 
 printName(library);
 
+
 //5. Write a JavaScript program to get the volume of a Cylinder with four decimal places using object classes. Volume of a cylinder : V = πr2h where r is the radius and h is the height of the cylinder. Try To Attempt : Difficult Level Increased
 class Cylinder {
   constructor(height, radius) {
@@ -63,6 +67,7 @@ let h = 12;
 let r = 3.3;
 let cy = new Cylinder(h, r);
 console.log(cy.area().toFixed(4));
+
 
 //6. Write a JavaScript program to sort an array of JavaScript objects.  Sample Object : var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }]; Expected Output: [[object Object] { author: "Walter Isaacson", libraryID: 4264, title: "Steve Jobs" }, [object Object] { author: "Suzanne Collins", libraryID: 3245, title: "Mockingjay: The Final Book of The Hunger Games" }, [object Object] { author: "The Road Ahead", libraryID: 1254, title: "Bill Gates" }]
 
