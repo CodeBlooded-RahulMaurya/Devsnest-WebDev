ae033c41eb365a435775028e073fc232662d3c9f
CodeBlooded-RahulMaurya <rahulmaurya0001@gmail.com>
2021-06-18 15:51:04 +0530

    fixed grid issue & added hover effect

--- Day-09-CSS-Challenge/CSStask/script.js
+++ Day-09-CSS-Challenge/CSStask/script.js
@@ -1,14 +1,14 @@
 const seatingArea = document.querySelector(".seating-area");
 function makeSeat() {
   const div = document.createElement("div");
-  div.style.height = "20px";
-  div.style.width = "20px";
-  div.style.backgroundColor = "red";
+  // div.style.height = "15%";
+  // div.style.width = "15%";
+  div.className = "seat";
+  div.style.backgroundColor = "#8B3F2C";
   div.style.zIndex = "1";
-  div.style.border = "2px solid black";
   return div;
 }
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 36; i++) {
   seatingArea.appendChild(makeSeat());
 }
